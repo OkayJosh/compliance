74fa1b154fb6041d7492f4ed89ab380f787ebbaa
Joshua Olatunji <kayodejoshua169@yahoo.com>
2024-10-17 19:51:57 +0100

    sumsub config

---
+++ sumsub/apps.py
@@ -0,0 +1,12 @@
+"""
+Application configuration for sumsub.
+"""
+from django.apps import AppConfig
+
+
+class SumsubConfig(AppConfig):
+    """
+    SumSub application configuration.
+    """
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sumsub'
